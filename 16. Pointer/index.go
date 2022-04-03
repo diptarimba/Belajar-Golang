5b335faf973a459b2db0a4c14151b71d52f822a4
diptarimba <vipalkuda@gmail.com>
2022-04-03 22:49:56 +0700

    menambahkan pointer di param function

--- 16. Pointer/index.go
+++ 16. Pointer/index.go
@@ -11,4 +11,18 @@ func main(){
 
 	fmt.Println("Nilai B\t:", *numberB)
 	fmt.Println("Nilai B\t:", numberB)
+
+	// numberA = 5
+	change(&numberA, 10)
+
+	fmt.Println("Nilai A\t:", numberA)
+	fmt.Println("Nilai A\t:", &numberA)
+
+	fmt.Println("Nilai B\t:", *numberB)
+	fmt.Println("Nilai B\t:", numberB)
+
+}
+
+func change(original *int, value int){
+	*original = value
 }
