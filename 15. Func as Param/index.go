ee5cb833c62ba596e947d2607633378dcaf854c3
diptarimba <vipalkuda@gmail.com>
2022-04-03 20:25:54 +0700

    func as param

---
+++ 15. Func as Param/index.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func main() {
+	var data = []string{"dipta", "jono", "kuni", "kono", "kano", "paino"}
+
+	var datane = filter(data, func(each string) bool {
+		return strings.Contains(each, "o")
+	})
+
+	var theteg = filter(data, func(each string) bool{
+		return len(each) == 4
+	})
+
+	fmt.Println("Data mengandung huruf o :", datane)
+	fmt.Printf("Yang hurufnya 4 adalah %v", theteg)
+}
+
+func filter(data []string, callback func(string) bool) []string {
+	var result []string
+	for _, each := range data {
+		if filtered := callback(each); filtered {
+			result = append(result, each)
+		}
+	}
+
+	return result
+}
